cae1ad4e80976c3963d79d5ce4c512900a8033be
BT_1N3_05 <sample@bths.edu>
2024-01-12 11:04:10 -0500

    help

--- src/Hunter.java
+++ src/Hunter.java
@@ -64,7 +64,7 @@ public class Hunter {
                     if (choice.equals("c")) {
                         System.out.print(Colors.RED + "Which dragon? " + Colors.RESET);
                         System.out.println(Room.dragonInfo(SCANNER.nextLine()));
-                        System.out.println(Room.getnames() + " wait for you to do something.");
+                        System.out.println(Colors.GREEN + Room.getnames() + " wait for you to attack." + Colors.GREEN);
                     }
                 } else {
                     onedragon();
